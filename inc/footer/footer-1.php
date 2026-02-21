08d4a07dac37a8781ea63e6055f7c30e8d40105a
elvira-platas <mailbox.elvira@gmail.com>
2026-02-21 19:46:01 +0400

    Update footer copyright to Elvira

--- inc/footer/footer-1.php
+++ inc/footer/footer-1.php
@@ -10,17 +10,7 @@ function kilka_footer_style_1(){ ?>
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="copyright">
-					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kilka' ) ); ?>">
-						<?php
-						/* translators: %s: CMS name, i.e. WordPress. */
-						printf( esc_html__( 'Proudly powered by %s', 'kilka' ), 'WordPress' );
-						?>
-					</a>
-					<span class="sep"> | </span>
-						<?php
-						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'kilka' ), 'kilka', 'ashathemes' );
-						?>
+					&copy; <?php echo date('Y'); ?> Мой сайт. Разработано с ❤️ Elvira (Тема Kilka).
 				</div>
 			</div>
 		</div>
