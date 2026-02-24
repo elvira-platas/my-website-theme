40e8686a2179f525767700d5696e44c7a861007d
elvira-platas <mailbox.elvira@gmail.com>
2026-02-24 21:44:50 +0400

    feat: implement compact hamburger menu aligned to the right, hide on mobile

--- inc/header/header-1.php
+++ inc/header/header-1.php
@@ -13,36 +13,40 @@ function kilka_header_style_1(){ ?>
 	        </div>
         <?php endif; ?>
 		<div class="container">
-			<div class="row">
+			<div class="row align-items-center">
 				<div class="col-lg-12">
-					<div class="site-branding text-center">
-						<?php
-						the_custom_logo();
-						if ( is_front_page() && is_home() ) :
-							?>
-							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<div class="header-main-flex">
+						<div class="site-branding text-center">
 							<?php
-						else :
-							?>
-							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-							<?php
-						endif;
-						$kilka_description = get_bloginfo( 'description', 'display' );
-						if ( $kilka_description || is_customize_preview() ) :
-							?>
-							<p class="site-description"><?php echo esc_html($kilka_description); ?></p>
+							the_custom_logo();
+							if ( is_front_page() && is_home() ) :
+								?>
+								<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+								<?php
+							else :
+								?>
+								<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+								<?php
+							endif;
+							$kilka_description = get_bloginfo( 'description', 'display' );
+							if ( $kilka_description || is_customize_preview() ) :
+								?>
+								<p class="site-description"><?php echo esc_html($kilka_description); ?></p>
+							<?php endif; ?>
+						</div><!-- .site-branding -->
+						
+						<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
+							<div class="kilka-responsive-menu"></div>
 						<?php endif; ?>
-					</div><!-- .site-branding -->
+					</div>
 				</div>
 			</div>
 		</div>
 	</header><!-- #masthead -->
-	<section class="mainmenu-area">
+	<section class="mainmenu-area" style="display:none;">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12">
-					<div class="kilka-responsive-menu"></div>
-					<button class="screen-reader-text menu-close"><?php esc_html_e( 'Close Menu', 'kilka' ); ?></button>
 					<div class="mainmenu">
 						<?php
 							wp_nav_menu( array(
