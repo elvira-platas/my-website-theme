a4f0e7cfbc72625f20302b51feaee789d1d23cb8
elvira-platas <262841397+elvira-platas@users.noreply.github.com>
2026-08-30 20:14:01 +0400

    Refine post and archive navigation

--- single.php
+++ single.php
@@ -25,7 +25,12 @@ get_header();
 						the_post();
 
 						get_template_part( 'template-parts/content', get_post_type() );
-						the_post_navigation();
+						the_post_navigation(
+							array(
+								'prev_text' => '<span class="screen-reader-text">' . esc_html__( 'Previous post:', 'kilka' ) . ' </span><span class="nav-title">%title</span>',
+								'next_text' => '<span class="screen-reader-text">' . esc_html__( 'Next post:', 'kilka' ) . ' </span><span class="nav-title">%title</span>',
+							)
+						);
 						// If comments are open or we have at least one comment, load up the comment template.
 						if ( comments_open() || get_comments_number() ) :
 							comments_template();
